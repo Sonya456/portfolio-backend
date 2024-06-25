364f1548ea0a50727b41d4822c77a927afb714ae
Sonya456 <126523183+Sonya456@users.noreply.github.com>
2024-06-25 18:43:13 +0200

    Update settings.py

--- portfolio_backend/settings.py
+++ portfolio_backend/settings.py
@@ -13,8 +13,7 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'lizka8456@gmail.com'
-EMAIL_HOST_PASSWORD = 'lhqv vanb lacz buea'
-EMAIL_USE_T = True
+EMAIL_HOST_PASSWORD = '**** **** **** ****'
 EMAIL_USE_SSL = False
 
 
